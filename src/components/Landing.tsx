e675cce9d03b4950c60c1a017ce2e9c56b4050b9
Joey <19317273+JWW127@users.noreply.github.com>
2022-09-21 02:58:18 -0500

    small content addition, styles change for mobile

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -20,25 +20,26 @@ export const Landing = () => {
 
   return (
     <div className="flex justify-around flex-wrap p-6 max-h-[1080px] bg-[#fcfcef] border-4 rounded-md shadow-lg shadow-gray-700 border-gray-500">
-      <h1 className="w-full text-center mt-8 font-serif text-bold text-3xl md:text-4xl lg:text-5xl">
-        Signet
+      <h1 className="w-full mt-8 font-serif text-5xl text-center underline text-bold lg:text-5xl">
+        SIGNET
       </h1>
-      <p className="mt-6 text-xl text-center basis-full w-full">{brand}</p>
-      <div className="mb-6 mt-6">{qr}</div>
+        <p className="font-serif text-2xl">Jewelers</p>
+      <p className="w-full mt-6 text-xl text-center basis-full">{brand}</p>
+      <div className="mt-6 mb-6">{qr}</div>
       {store ? (
-        <p className=" text-xl text-center basis-full w-full">
+        <p className="w-full text-xl text-center basis-full">
           Join <span className="text-blue-700 text-bold">{store}</span> Today
         </p>
       ) : (
-        <p className="mt-6 text-center basis-full w-full">Enter Job Location</p>
+        <p className="w-full mt-6 text-center basis-full">Enter Job Location</p>
       )}
 
-      <form className="basis-full md:basis-11/12 lg:basis-11/12 w-full text-center">
+      <form className="w-full text-center basis-full md:basis-11/12 lg:basis-11/12">
         <input
           type="text"
           placeholder="https://example.com"
           onChange={(e) => setUrl(e.target.value)}
-          className="border-2 border-gray-300 rounded-sm w-full text-center mb-3"
+          className="w-full mb-3 text-center border-2 border-gray-300 rounded-sm"
         />
       </form>
     </div>
